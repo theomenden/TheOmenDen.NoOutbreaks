b0050e2223cfa708eb60e05ddbdaef3d3aa07b92
Andrew Diego Beers <andrew.d.beers@gmail.com>
2024-09-26 21:29:06 -0400

    Add in architectury.common.json Add in Architectury support. Support NeoForge Adjust Mod implementations Add in various translations Remove original fabric.mod.json Change VillageSiegeMixin.java to use @ModifyVariable instead of @Inject for the tick method. Hoping this will be more precise - though we are querying the server every tick.

--- common/src/main/java/org/theomenden/nooutbreaks/mixins/VillageSiegeMixin.java
+++ common/src/main/java/org/theomenden/nooutbreaks/mixins/VillageSiegeMixin.java
@@ -1,40 +1,31 @@
 package org.theomenden.nooutbreaks.mixins;
 
-import net.minecraft.server.level.ServerLevel;
+import dev.architectury.utils.GameInstance;
 import net.minecraft.world.entity.ai.village.VillageSiege;
 import net.minecraft.world.level.CustomSpawner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.world.level.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.theomenden.nooutbreaks.main.NoOutbreaks;
+
+import java.util.Objects;
 
 @Mixin(VillageSiege.class)
 public abstract class VillageSiegeMixin implements CustomSpawner {
-    @Shadow private VillageSiege.State siegeState;
-    @Shadow private boolean hasSetupSiege;
 
-    @Unique
-    private static final Logger LOGGER = LoggerFactory.getLogger(NoOutbreaks.class);
 
-    @Inject(method = "tick(Lnet/minecraft/server/level/ServerLevel;ZZ)I", at = @At("HEAD"), cancellable = true)
-    private void onTick(ServerLevel level, boolean spawnEnemies, boolean spawnFriendlies, CallbackInfoReturnable<Integer> cir) {
-        var gameRules = level.getGameRules();
+    @ModifyVariable(method = "tick", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private boolean onTick(boolean value) {
+        try{
+            var gameServer = Objects.requireNonNull(GameInstance.getServer());
+            var serverGameRules = gameServer.getGameRules();
 
-        boolean shouldNotStopZombieSiege = gameRules.getBoolean(NoOutbreaks.SHOULD_ZOMBIES_RAID);
-
-        if(shouldNotStopZombieSiege) {
-            return;
+            return serverGameRules.getBoolean(NoOutbreaks.SHOULD_ZOMBIES_RAID);
+        } catch(NullPointerException e){
+           NoOutbreaks.LOGGER.error("Unable to get {}, defaulting to supplied value", NoOutbreaks.RULE_NAME);
         }
-
-        LOGGER.info("Detected Zombie Siege");
-        this.siegeState = VillageSiege.State.SIEGE_DONE;
-        this.hasSetupSiege = false;
-        cir.setReturnValue(0);
-        LOGGER.info("Siege Stopped.");
+        return value;
     }
 }
